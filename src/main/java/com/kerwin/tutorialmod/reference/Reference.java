086e421bc4a2668ade1651cf2e7d6cdc5de68cac
Will Kerwin <10259319+Dr-gummybears@users.noreply.github.com>
2019-06-24 23:13:49 +0100

    Initial commit with the 1.12 Stuff

---
+++ src/main/java/com/kerwin/tutorialmod/reference/Reference.java
@@ -0,0 +1,11 @@
+package com.kerwin.tutorialmod.reference;
+
+public class Reference {
+
+    public static final String MODID = "tutorialmod";
+    public static final String NAME = "Tutorial Mod";
+    public static final String VERSION = "1.0";
+    public static final String CLIENT = " ";
+    public static final String SERVER = " ";
+
+}
