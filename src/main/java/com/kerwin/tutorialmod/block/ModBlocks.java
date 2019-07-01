05274fd98e95bcba0cb94b4726a2ae79d815d838
Will Kerwin <10259319+Dr-gummybears@users.noreply.github.com>
2019-07-02 00:53:20 +0100

    trying to do inputs and outputs it seems to be coded right but goes in to both input and output when inputted

--- src/main/java/com/kerwin/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/kerwin/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,5 @@
 package com.kerwin.tutorialmod.block;
 
-import com.kerwin.tutorialmod.reference.Reference;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
